a5b5b2181f9fc502352bd1e8b554a257f325ef17
baozj <www.1670370148@qq.com>
2025-12-02 19:57:57 +0800

    feat: 初始化

---
+++ src/views/VirtualTableTest/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="virtual-table-test">
+    <h1>VirtualTableTest</h1>
+  </div>
+</template>
+
+<style>
+.virtual-table-test {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+</style>
